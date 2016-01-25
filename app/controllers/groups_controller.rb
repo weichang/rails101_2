3caade6a8cf36bd15bb0b70c0bb1fbf3a5bb7b82
weicheng <a376836@gmail.com>
2016-01-25 09:44:24 +0000

    ADD Group CRUD

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,59 @@
 class GroupsController < ApplicationController
     
     def index
-         flash[:notice] = "早安！你好！"
+        @groups = Group.all
+    end
+    def show
+       @group = Group.find(params[:id])   
     end
     
+    def new
+        @group = Group.new
+    end
+    def create
+        
+        @group = Group.create(groups_params)
+        
+        if @group.save
+            
+            flash[:notice] = "群組新增成功!!"
+            redirect_to groups_path
+            
+        else
+            render :new
+        end
+        
+        
+    end
+    
+    def edit
+        @group = Group.find(params[:id])
+    end
+    
+    def update
+        @group = Group.find(params[:id])
+        
+        if @group.update(groups_params)
+            
+            flash[:notice] = '修改成功'
+            redirect_to groups_path
+        else
+            
+            render :edit
+        end
+        
+    end
+    
+    def destroy
+        @group = Group.find(params[:id])
+        @group.destroy
+        flash[:notice] = "刪除成功"
+        redirect_to groups_path
+    end
+    
+    private 
+    
+    def groups_params
+        params.require(:group).permit(:title,:description)
+    end
 end
